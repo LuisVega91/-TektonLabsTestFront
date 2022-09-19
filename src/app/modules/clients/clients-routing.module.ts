e00cc0b96d2dcd4fd2ba927b442713affd6214d3
Luis Enrique Vega Martinez <levega@bancolombia.com.co>
2022-09-19 04:42:08 -0500

    update clients module

--- src/app/modules/clients/clients-routing.module.ts
+++ src/app/modules/clients/clients-routing.module.ts
@@ -1,10 +1,26 @@
+import { defaultRoute } from './../../common/helpers/default-routes';
+import { LayoutComponent } from './components/layout/layout.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'genres',
+        loadChildren: () =>
+          import('../genres/genres.module').then((m) => m.GenresModule),
+      },
+      ...defaultRoute('genres/list'),
+    ],
+  },
+  ...defaultRoute('genres/list'),
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ClientsRoutingModule { }
+export class ClientsRoutingModule {}
--- src/app/modules/clients/clients.module.ts
+++ src/app/modules/clients/clients.module.ts
@@ -1,14 +1,12 @@
+import { MaterialModule } from './../../common/modules/material/material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ClientsRoutingModule } from './clients-routing.module';
-
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    ClientsRoutingModule
-  ]
+  declarations: [LayoutComponent],
+  imports: [CommonModule, ClientsRoutingModule, MaterialModule],
 })
-export class ClientsModule { }
+export class ClientsModule {}
