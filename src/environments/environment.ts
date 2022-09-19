b4cc5364d6869abe803bb7456516506a3946d600
Luis Enrique Vega Martinez <levega@bancolombia.com.co>
2022-09-19 02:01:01 -0500

    add environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'http://localhost:3000/api/v1',
+  apiKey: 'a1b2c33d4e5f6g7h8i9jakblc'
 };
 
 /*
