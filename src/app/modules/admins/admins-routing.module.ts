65f14c0148de03c2989a1c0ec78517476128eca7
Luis Enrique Vega Martinez <levega@bancolombia.com.co>
2022-09-19 04:41:52 -0500

    update admins module

--- src/app/modules/admins/admins-routing.module.ts
+++ src/app/modules/admins/admins-routing.module.ts
@@ -1,10 +1,36 @@
+import { defaultRoute } from './../../common/helpers/default-routes';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path:'genres',
+        loadChildren: () =>
+          import('../genres/genres.module').then((m) => m.GenresModule),
+      },
+      {
+        path:'songs',
+        loadChildren: () =>
+          import('../songs/songs.module').then((m) => m.SongsModule),
+      },
+      {
+        path:'users',
+        loadChildren: () =>
+          import('../users/users.module').then((m) => m.UsersModule),
+      },
+      ...defaultRoute('genres/list'),
+    ],
+  },
+  ...defaultRoute('genres/list'),
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AdminsRoutingModule { }
+export class AdminsRoutingModule {}
--- src/app/modules/admins/admins.module.ts
+++ src/app/modules/admins/admins.module.ts
@@ -1,14 +1,12 @@
+import { MaterialModule } from './../../common/modules/material/material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminsRoutingModule } from './admins-routing.module';
-
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    AdminsRoutingModule
-  ]
+  declarations: [LayoutComponent],
+  imports: [CommonModule, AdminsRoutingModule, MaterialModule],
 })
-export class AdminsModule { }
+export class AdminsModule {}
